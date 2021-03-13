6dd8b2bd12f8bd5068915fb5983608ff18254d8f
Danny <agyapongdaniel7777@gmail.com>
2021-03-13 13:36:45 +0000

    build initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 function App() {
   return (
-    <div className="bg-blue-50 ">
-      <nav className="p-x20"></nav>
-      <h2 className=" font-custom ">Daniel Agyapong s</h2>
-      <button class="btn btn-green">Button</button>
+    <div className=" bg-navy text-white">
+      {/* 0A192F */}
+      <nav className="w-full">
+        
+        <ul className="flex flex-row justify-end content-between">
+          <li class="fadedown-enter-done">
+            <a href="/#about">About</a>
+          </li>
+          <li class="fadedown-enter-done">
+            <a href="/#jobs">Experience</a>
+          </li>
+          <li class="fadedown-enter-done">
+            <a href="/#projects">Work</a>
+          </li>
+          <li class="fadedown-enter-done">
+            <a href="/#contact">Contact</a>
+          </li>
+          <li>
+            <button>Resume</button>
+          </li>
+        </ul>
+      </nav>
+      <h1>Hello, I am</h1>
+      <h2>Daniel Agyapong</h2>
+      <h3>I build professional cross-platform apps</h3>
+      <p>
+        I build mobile, web and desktop apps. I love to help open-source
+        community during my free times.
+      </p>
+
+      <a href="mailto:agyapongdaniel7777@gmail.com">Contact Me</a>
     </div>
   );
 }
