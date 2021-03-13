b32e91811b04583a4407875298b0b42741cca843
Danny <agyapongdaniel7777@gmail.com>
2021-03-13 12:40:14 +0000

    apply font-custom to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="bg-blue-50 ">
       <nav className="p-x20"></nav>
-      <h2 className=" ">Daniel Agyapong s</h2>
+      <h2 className=" font-custom ">Daniel Agyapong s</h2>
       <button class="btn btn-green">Button</button>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      custom: [
+        "Calibre",
+        "San Francisco",
+        "SF Pro Text",
+        "-apple-system",
+        "system-ui",
+        "sans-serif",
+      ],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
