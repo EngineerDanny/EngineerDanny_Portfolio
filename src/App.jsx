4cc5293326c691cb69384df60a49cff16c56995a
Danny <agyapongdaniel7777@gmail.com>
2021-03-14 20:23:24 +0000

    little comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className=" bg-navy text-white box-border mx-auto">
-      <section className="min-h-screen">
+      {/* Home Section */}
+      <section className="h-screen">
         <nav className="flex flex-row p-5 items-center justify-between">
           <img src="@/public/assets/my-pic.jpg" className="" alt="MyPicture" />
           <ul className="flex flex-row items-center">
@@ -46,42 +47,43 @@ function App() {
         </div>
       </section>
 
-      <section className="min-h-screen">
+      {/* About section */}
+      <section className="h-screen">
         <div className="flex flex-col justify-evenly h-4/6">
-          {/* About section */}
           <div className="flex flex-row items-center">
-            <div className="w-40 h-0.5 bg-white"></div>
-            <h1 className="text-4xl font-medium opacity-80">About</h1>
-            <div className="w-40 h-0.5 bg-white"></div>
+            <div className="w-40 h-0.5  bg-gray-300 opacity-30"></div>
+            <h1 className="text-4xl font-medium opacity-80 mx-3">About</h1>
+            <div className="w-40 h-0.5 bg-gray-300 opacity-30"></div>
           </div>
           {/* Details */}
           <div className="flex flex-row w-4/5 items-center">
             <p className="max-w-md text-base font-light opacity-80">
               Hi! I'm Daniel, a FullStack engineer based in KNUST, Ghana. I
               enjoy creating things that live on the internet, whether that be
-              websites, applications, or anything in between
-            </p>
-
-            <p className="max-w-md text-base font-light opacity-80">
+              websites, applications, or anything in between. <br />
+              <br />
               My goal is to always build products that provide pixel-perfect,
               performant experiences. Shortly after graduating from Northeastern
               University, I joined the engineering team at Upstatement where I
               work on a wide variety of interesting and meaningful projects on a
               daily basis.
-            </p>
-            <p className="max-w-md text-base font-light opacity-80">
-              Here are a few technologies I've been working with recently:
+              <br />
+              <br /> Here are a few technologies I've been working with
+              recently:
             </p>
           </div>
         </div>
       </section>
-
-      <section className="min-h-screen">
+      {/* Work Experience Section */}
+      <section className="h-screen">
         <div className="flex flex-col justify-evenly h-4/6">
+          {/* About section */}
           <div className="flex flex-row items-center">
-            <div className="w-40 h-0.5 bg-white"></div>
-            <h1 className="text-4xl font-medium opacity-80">About</h1>
-            <div className="w-40 h-0.5 bg-white"></div>
+            <div className="w-40 h-0.5 bg-gray-300 opacity-30"></div>
+            <h1 className="text-4xl font-medium opacity-80 mx-3">
+              My Work Experience
+            </h1>
+            <div className="w-40 h-0.5 bg-gray-300 opacity-30"></div>
           </div>
 
           <div className="flex flex-row w-4/5 items-center">
