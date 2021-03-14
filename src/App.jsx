f4ad84b7d17fc70a8462072990119e8411af11d7
Danny <agyapongdaniel7777@gmail.com>
2021-03-14 16:51:10 +0000

    layout restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className=" bg-navy text-white box-border h-screen mx-auto">
+    <div className=" bg-navy text-white box-border mx-auto">
       <nav className="flex flex-row p-5 items-center justify-between">
         <img src="@/public/assets/my-pic.jpg" className="" alt="MyPicture" />
 
@@ -25,19 +25,43 @@ function App() {
         </ul>
       </nav>
 
+      <section className="h-screen">
+        <div className="flex flex-col justify-evenly h-4/6">
+          <h1 className="text-md text-green-300">Hello, I am</h1>
+          <h2 className="text-6xl font-semibold">Daniel Agyapong</h2>
+          <h3 className="text-5xl font-medium opacity-80 -mt-4">
+            I build professional cross-platform apps
+          </h3>
+          <p className="text-base font-light max-w-lg opacity-80">
+            I am a FullStack Developer and Electrical/Electronics Engineer at
+            KNUST specialized in building performant mobile, web and desktop
+            apps. I love to help open-source community.
+          </p>
+
+          <a
+            href="mailto:agyapongdaniel7777@gmail.com"
+            className="text-base p-2 border-2 border-green-500 rounded-md text-green-500 w-36 text-center"
+          >
+            Contact Me
+          </a>
+        </div>
+      </section>
+
       <section className="flex flex-col justify-evenly h-4/6">
-        <h1 className="text-sm text-green-300">Hello, I am</h1>
-        <h2 className="text-6xl">Daniel Agyapong</h2>
-        <h3 className=" text-5xl">I build professional cross-platform apps</h3>
-        <p className="max-w-md">
-          I am a FullStack Developer and Electrical/Electronics Engineer in
+        <h1 className="text-md text-green-300">Hello, I am</h1>
+        <h2 className="text-6xl font-semibold">Daniel Agyapong</h2>
+        <h3 className="text-5xl font-medium opacity-80 -mt-4">
+          I build professional cross-platform apps
+        </h3>
+        <p className="text-base font-light max-w-lg opacity-80">
+          I am a FullStack Developer and Electrical/Electronics Engineer at
           KNUST specialized in building performant mobile, web and desktop apps.
-          I love to help open-source community during my free times.
+          I love to help open-source community.
         </p>
 
         <a
           href="mailto:agyapongdaniel7777@gmail.com"
-          className="text-base px-2 py-1 border-2 border-green-500 rounded-md text-green-500 w-36 text-center"
+          className="text-base p-2 border-2 border-green-500 rounded-md text-green-500 w-36 text-center"
         >
           Contact Me
         </a>
