e721bf4746add867920c45ee2db5529757e8e821
Danny <agyapongdaniel7777@gmail.com>
2021-03-14 15:44:44 +0000

    set list items hover state features

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ function App() {
         <img src="@/public/assets/my-pic.jpg" className="" alt="MyPicture" />
 
         <ul className="flex flex-row items-center">
-          <li class="mx-2">
+          <li class="text-sm mx-2 hover:text-green-500">
             <a href="/#about">About</a>
           </li>
-          <li class="mx-2">
+          <li class="text-sm mx-2 hover:text-green-500">
             <a href="/#jobs">Experience</a>
           </li>
-          <li class="mx-2">
+          <li class="text-sm mx-2 hover:text-green-500">
             <a href="/#projects">Work</a>
           </li>
-          <li class="mx-2">
+          <li class="text-sm mx-2 hover:text-green-500">
             <a href="/#contact">Contact</a>
           </li>
           <li class="mx-2">
-            <button className=" px-2 py-1 border-2 border-green-700  rounded-md text-green-700">
+            <button className=" px-2 py-1 border-2 border-green-500   rounded-md text-green-500">
               Resume
             </button>
           </li>
