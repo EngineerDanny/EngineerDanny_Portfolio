974bcabb98494ee082fd02e15b037eef33eef658
Danny <agyapongdaniel7777@gmail.com>
2021-03-14 16:22:01 +0000

    main page style update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 function App() {
   return (
     <div className=" bg-navy text-white box-border h-screen mx-auto">
-
       <nav className="flex flex-row p-5 items-center justify-between">
         <img src="@/public/assets/my-pic.jpg" className="" alt="MyPicture" />
 
@@ -26,19 +25,23 @@ function App() {
         </ul>
       </nav>
 
-      <section className="flex flex-col justify-evenly h-2/5">
+      <section className="flex flex-col justify-evenly h-4/6">
         <h1 className="text-sm text-green-300">Hello, I am</h1>
         <h2 className="text-6xl">Daniel Agyapong</h2>
         <h3 className=" text-5xl">I build professional cross-platform apps</h3>
-        <p>
-          I build mobile, web and desktop apps. I love to help open-source
-          community during my free times.
+        <p className="max-w-md">
+          I am a FullStack Developer and Electrical/Electronics Engineer in
+          KNUST specialized in building performant mobile, web and desktop apps.
+          I love to help open-source community during my free times.
         </p>
 
-        <a href="mailto:agyapongdaniel7777@gmail.com">Contact Me</a>
+        <a
+          href="mailto:agyapongdaniel7777@gmail.com"
+          className="text-base px-2 py-1 border-2 border-green-500 rounded-md text-green-500 w-36 text-center"
+        >
+          Contact Me
+        </a>
       </section>
-
-
     </div>
   );
 }
