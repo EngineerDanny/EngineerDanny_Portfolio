f5fdc6cc9f4c9caede1038fa702e7fb855557195
Danny <agyapongdaniel7777@gmail.com>
2021-03-14 13:08:18 +0000

    delete react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 function App() {
   return (
-    <div className=" bg-navy text-white box-border max-w-full">
+    <div className=" bg-navy text-white box-border h-screen">
       {/* 0A192F */}
       <nav className="w-full flex flex-row p-5">
-        
-        <img src="./assets/my-pic.jpg" className=" h-10 w-10 object-cover" alt="MyPicture"/>
+        <img
+          src="./assets/my-pic.jpg"
+          className=" h-10 w-10 object-cover"
+          alt="MyPicture"
+        />
         <ul className=" w-10 flex flex-row">
           <li class="fadedown-enter-done">
             <a href="/#about">About</a>
@@ -23,15 +26,18 @@ function App() {
           </li>
         </ul>
       </nav>
-      <h1>Hello, I am</h1>
-      <h2>Daniel Agyapong</h2>
-      <h3>I build professional cross-platform apps</h3>
-      <p>
-        I build mobile, web and desktop apps. I love to help open-source
-        community during my free times.
-      </p>
 
-      <a href="mailto:agyapongdaniel7777@gmail.com">Contact Me</a>
+      <body className="">
+        <h1>Hello, I am</h1>
+        <h2>Daniel Agyapong</h2>
+        <h3>I build professional cross-platform apps</h3>
+        <p>
+          I build mobile, web and desktop apps. I love to help open-source
+          community during my free times.
+        </p>
+
+        <a href="mailto:agyapongdaniel7777@gmail.com">Contact Me</a>
+      </body>
     </div>
   );
 }
