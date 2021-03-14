11df3e78e17aaf4867d7541b2bc94f4714e35a7b
Danny <agyapongdaniel7777@gmail.com>
2021-03-14 18:27:52 +0000

    use paragraphs to refactor breaks

--- src/App.jsx
+++ src/App.jsx
@@ -59,16 +59,18 @@ function App() {
             <p className="max-w-md text-base font-light opacity-80">
               Hi! I'm Daniel, a FullStack engineer based in KNUST, Ghana. I
               enjoy creating things that live on the internet, whether that be
-              websites, applications, or anything in between. <br />
-              <br />
+              websites, applications, or anything in between
+            </p>
+
+            <p className="max-w-md text-base font-light opacity-80">
               My goal is to always build products that provide pixel-perfect,
               performant experiences. Shortly after graduating from Northeastern
               University, I joined the engineering team at Upstatement where I
               work on a wide variety of interesting and meaningful projects on a
               daily basis.
-              <br />
-              <br /> Here are a few technologies I've been working with
-              recently:
+            </p>
+            <p className="max-w-md text-base font-light opacity-80">
+              Here are a few technologies I've been working with recently:
             </p>
           </div>
         </div>
@@ -76,13 +78,12 @@ function App() {
 
       <section className="min-h-screen">
         <div className="flex flex-col justify-evenly h-4/6">
-          
           <div className="flex flex-row items-center">
             <div className="w-40 h-0.5 bg-white"></div>
             <h1 className="text-4xl font-medium opacity-80">About</h1>
             <div className="w-40 h-0.5 bg-white"></div>
           </div>
-       
+
           <div className="flex flex-row w-4/5 items-center">
             <p className="max-w-md text-base font-light opacity-80"></p>
           </div>
