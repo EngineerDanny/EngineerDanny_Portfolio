3bffc6d3175addee927582cae039ccc04bbbff5c
Danny <agyapongdaniel7777@gmail.com>
2021-03-15 06:29:13 +0000

    IntroHeader init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import logo from "./assets/my-pic.jpg";
 import NavBar from "./components/NavBar";
+import IntroHeader from "./components/IntroHeader";
 
 function App() {
   return (
@@ -7,24 +7,7 @@ function App() {
       {/* Home Section */}
       <section className="h-screen">
         <NavBar />
-        <div className="flex flex-col justify-evenly h-4/6">
-          <h1 className="text-md text-green-300">Hello, I am</h1>
-          <h2 className="text-6xl font-semibold">Daniel Agyapong.</h2>
-          <h3 className="text-5xl font-medium opacity-80 -mt-4">
-            I build professional cross-platform apps.
-          </h3>
-          <p className="text-base font-light max-w-lg opacity-80">
-            I am a FullStack Developer and Electrical/Electronics Engineer at
-            KNUST specialized in building performant mobile, web and desktop
-            apps. I love to help open-source community.
-          </p>
-          <a
-            href="mailto:agyapongdaniel7777@gmail.com"
-            className="text-base p-2 border-2 border-green-500 rounded-md text-green-500 w-36 text-center"
-          >
-            Contact Me
-          </a>
-        </div>
+        <IntroHeader/>  
       </section>
 
       {/* About Section */}
---
+++ src/components/IntroHeader.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import profilePic from "../assets/my-pic.jpg"
+
+const IntroHeader = () => {
+  return (
+    <div className="flex flex-row  justify-between">
+      <div className="flex flex-col justify-evenly h-4/6">
+        <h1 className="text-md text-green-300">Hello, I am</h1>
+        <h2 className="text-6xl font-semibold">Daniel Agyapong.</h2>
+        <h3 className="text-5xl font-medium opacity-80 -mt-4">
+          I build professional cross-platform apps.
+        </h3>
+        <p className="text-base font-light max-w-lg opacity-80">
+          I am a FullStack Developer and Electrical/Electronics Engineer at
+          KNUST specialized in building performant mobile, web and desktop apps.
+          I love to help open-source community.
+        </p>
+        <a
+          href="mailto:agyapongdaniel7777@gmail.com"
+          className="text-base p-2 border-2 border-green-500 rounded-md text-green-500 w-36 text-center"
+        >
+          Contact Me
+        </a>
+      </div>
+      <img src={profilePic} className="w-48 h-48 rounded-3xl object-cover" alt="MyPicture" />
+    </div>
+  );
+};
+
+export default IntroHeader;
