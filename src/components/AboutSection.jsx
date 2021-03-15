24a47850c82985d75fdc8b072ec83c552afcc75a
Danny <agyapongdaniel7777@gmail.com>
2021-03-15 19:03:52 +0000

    AboutSection style update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,15 +8,15 @@ import node from "../assets/techs/node.png";
 const AboutSection = () => {
   const techImages = [express, mongo, node,react];
   return (
-    <div className="flex flex-col justify-evenly h-4/6">
-      <div className="flex flex-row items-center">
+    <div className="flex flex-col items-center h-4/6">
+      <div className="flex flex-row items-center my-10">
         <div className="w-40 h-0.5  bg-gray-300 opacity-30"></div>
         <h1 className="text-4xl font-medium opacity-80 mx-3">About</h1>
         <div className="w-40 h-0.5 bg-gray-300 opacity-30"></div>
       </div>
       {/* Details */}
-      <div className="flex flex-row w-4/5 items-center">
-        <p className="max-w-md text-base font-light opacity-80">
+      <div className="flex flex-col  items-center">
+        <p className=" w-4/6 text-base font-light opacity-80 mb-12">
           Hi! I'm Daniel, a FullStack engineer based in KNUST, Ghana. I enjoy
           creating things that live on the internet, whether that be websites,
           applications, or anything in between. <br />
@@ -31,7 +31,7 @@ const AboutSection = () => {
         </p>
 
         <div>
-          <div className="flex flex-wrap justify-center">
+          <div className="flex  justify-center">
             {techImages.map((tech, index) => (
               <span
                 key={index}
