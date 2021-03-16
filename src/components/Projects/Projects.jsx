fe23405ed1e85498bbc561f6beb79222bfb88d2d
Danny <agyapongdaniel7777@gmail.com>
2021-03-16 07:55:10 +0000

    My Projects Section init

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div className="flex flex-col h-4/6 items-center justify-start">
+        {/* Work Experience Header */}
+        <div className="flex flex-row items-center my-10">
+          <div className="w-40 h-0.5 bg-gray-300 opacity-30"></div>
+          <h1 className="text-4xl font-medium opacity-80 mx-3">
+            My Projects
+          </h1>
+          <div className="w-40 h-0.5 bg-gray-300 opacity-30"></div>
+        </div>
+  
+      </div>
+    )
+}
+
+export default Projects
