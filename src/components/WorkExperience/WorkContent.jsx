77fa950ce83a5629ea6ee4fe9c577db16bd07e50
Danny <agyapongdaniel7777@gmail.com>
2021-03-16 07:15:00 +0000

    simplify contentList

--- src/components/WorkExperience/WorkContent.jsx
+++ src/components/WorkExperience/WorkContent.jsx
@@ -3,23 +3,13 @@ import React from "react";
 const WorkContent = ({ title, duration, contentList }) => {
   return (
     <div className="flex flex-col">
-      <h1>Engineer</h1>
+      <h1>title</h1>
       {/* Job duration */}
-      <p className="">May 2018 - Present</p>
+      <p className="">duration</p>
       <ul>
-        <li>
-          Write modern, performant, maintainable code for a diverse array of
-          client and internal projects
-        </li>
-        <li>
-          Work with a variety of different languages, platforms, frameworks, and
-          content management systems such as JavaScript, TypeScript, Gatsby,
-          React, Craft, WordPress, Prismic, and Netlify
-        </li>
-        <li>
-          Communicate with multi-disciplinary teams of engineers, designers,
-          producers, and clients on a daily basis
-        </li>
+        {contentList.map((item, index) => (
+          <li key={index}>item</li>
+        ))}
       </ul>
     </div>
   );
