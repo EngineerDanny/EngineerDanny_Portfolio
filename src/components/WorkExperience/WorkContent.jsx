4b313b298c181e0461f80bf162e19e27f0cbb4eb
Danny <agyapongdaniel7777@gmail.com>
2021-03-16 07:16:48 +0000

    fix syntax issues

--- src/components/WorkExperience/WorkContent.jsx
+++ src/components/WorkExperience/WorkContent.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const WorkContent = ({ title, duration, contentList }) => {
   return (
     <div className="flex flex-col">
-      <h1>title</h1>
+      <h1>{title}</h1>
       {/* Job duration */}
-      <p className="">duration</p>
+      <p className="">{duration}</p>
       <ul>
         {contentList.map((item, index) => (
-          <li key={index}>item</li>
+          <li key={index}>{item}</li>
         ))}
       </ul>
     </div>
