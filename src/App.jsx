6ad7e05c7cd16a726d796c3e98fb258fd1f2b016
Danny <agyapongdaniel7777@gmail.com>
2021-03-19 08:15:45 +0000

    inject Routes into App

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,42 @@ import AboutSection from "./components/About/AboutSection";
 import WorkExperience from "./components/WorkExperience/WorkExperience";
 import Projects from "./components/Projects/Projects";
 import ContactSideBar from "./components/ContactSideBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className=" bg-navy text-white box-border mx-auto">
-      {/* Home Section */}
-      {/* <div className="flex flex-row" >
-        
-      </div> */}
+    <Router>
+      <div className=" bg-navy text-white box-border mx-auto">
+        <Switch>
+          <Route path="/about">
+            <section className="h-screen flex flex-row items-center">
+              <ContactSideBar />
+              <IntroHeader />
+            </section>
+          </Route>
 
-      <NavBar />
-      <section className="h-screen flex flex-row items-center">
-        <ContactSideBar />
-        <IntroHeader />
-      </section>
+          <Route path="/users">
+            <section className="">
+              <AboutSection />
+            </section>
+          </Route>
 
-      {/* About Section */}
-      {/* <section className="">
-        <AboutSection />
-      </section> */}
+          <Route path="/">
+            <section className="h-screen">
+              <WorkExperience />
+            </section>
+          </Route>
 
-      {/* Work Experience Section */}
-      {/* <section className="h-screen">
-        <WorkExperience />
-      </section> */}
+          <Route path="/">
+            <section className="h-screen">
+              <Projects />
+            </section>
+          </Route>
+        </Switch>
 
-      {/* Projects Section */}
-      {/* <section className="h-screen">
-        <Projects />
-      </section> */}
-    </div>
+        <NavBar />
+      </div>
+    </Router>
   );
 }
 
