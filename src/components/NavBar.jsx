907f3ad80b3c6a8f06692e690ca3899587350fc1
Danny <agyapongdaniel7777@gmail.com>
2021-03-19 02:36:45 +0000

    Resume btn re-style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,11 @@ import logo from "../assets/my-pic.jpg";
 const NavBar = () => {
   return (
     <nav className="flex flex-row p-5 items-center justify-between">
-      <img src={logo} className=" w-10 h-10 rounded-3xl object-cover" alt="MyPicture" />
+      <img
+        src={logo}
+        className=" w-10 h-10 rounded-3xl object-cover"
+        alt="MyPicture"
+      />
       <ul className="flex flex-row items-center">
         <li class="text-sm mx-2 text-gray-300 hover:text-green-500">
           <a href="/#about">About</a>
@@ -19,7 +23,8 @@ const NavBar = () => {
           <a href="/#contact">Contact</a>
         </li>
         <li class="mx-2">
-          <button className="text-sm px-2 py-1 border-2 border-green-500 rounded-md text-green-500">
+          <button className="text-sm px-4 py-2 border-2 border-green-500 
+          rounded-full text-green-500 hover:bg-green-500 hover:text-white">
             Resume
           </button>
         </li>
