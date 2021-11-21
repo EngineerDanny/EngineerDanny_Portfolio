2d2d1d9fd76306420240cfc70ac80fc746273355
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-11-21 14:27:48 +0000

    TechTile component UI update

--- src/components/About/TechTile.jsx
+++ src/components/About/TechTile.jsx
@@ -3,15 +3,13 @@ import { motion } from "framer-motion";
 
 const TechTile = ({ title, percentage }) => {
   return (
-    <tr className="flex flex-row mb-6 h-5">
-      <td className="w-1/12 mr-3 self-center text-xs font-bold opacity-80">
-        {title}
-      </td>
+    <tr className="flex flex-row mb-4 h-2">
+    
       <td className="flex rounded-lg bg-white w-full">
         <motion.div
           animate={{width: percentage}}
           transition={{ ease: "easeOut", duration: 1 }}
-          className="rounded-l-lg origin-left bg-gradient-to-br from-green-200 to-green-900"
+          className="rounded-l-lg origin-left bg-gradient-to-br  from-indigo-300 to-indigo-500"
         ></motion.div>
       </td>
     </tr>
