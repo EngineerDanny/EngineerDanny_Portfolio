451ed8217790924eeed1fd81fff954220c888571
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-11-23 12:38:09 +0000

    update project model with new data

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -31,6 +31,21 @@ import shaq2 from "../../assets/appimages/shaq/2.webp";
 import shaq3 from "../../assets/appimages/shaq/3.webp";
 import shaq4 from "../../assets/appimages/shaq/4.webp";
 
+import hearts1 from "../../assets/appimages/hearts/1.webp";
+import hearts2 from "../../assets/appimages/hearts/2.webp";
+import hearts3 from "../../assets/appimages/hearts/3.webp";
+import hearts4 from "../../assets/appimages/hearts/4.webp";
+
+import lystn1 from "../../assets/appimages/lystn/1.webp";
+import lystn2 from "../../assets/appimages/lystn/2.webp";
+import lystn3 from "../../assets/appimages/lystn/3.webp";
+import lystn4 from "../../assets/appimages/lystn/4.webp";
+
+import godlives1 from "../../assets/appimages/godlives/1.webp";
+import godlives2 from "../../assets/appimages/godlives/2.webp";
+import godlives3 from "../../assets/appimages/godlives/3.webp";
+import godlives4 from "../../assets/appimages/godlives/4.webp";
+
 const projectModels = [
   {
     title: "Farmhouse Movies App",
@@ -59,7 +74,7 @@ const projectModels = [
       "your finger tips on your terms.",
     images: [aukiss1, aukiss2, aukiss3, aukiss4, aukiss5],
     google_play_link:
-      "https://play.google.com/store/apps/details?id=com.farmhousemovies.app",
+      "https://play.google.com/store/apps/details?id=com.aukiss.aukiss_app",
     apple_appstore_link:
       "https://apps.apple.com/us/app/farmhouse-movies/id1541154245",
   },
@@ -73,7 +88,43 @@ const projectModels = [
       "We made paying for your food/packages as simple as possible. That’s why you can pay conveniently and securely with your mobile money or credit card. Whatever works for you.",
     images: [shaq1, shaq2, shaq3, shaq4],
     google_play_link:
-      "https://play.google.com/store/apps/details?id=com.farmhousemovies.app",
+      "https://play.google.com/store/apps/details?id=com.shaqexpress.app",
+    apple_appstore_link:
+      "https://apps.apple.com/us/app/farmhouse-movies/id1541154245",
+  },
+
+  {
+    title: "Accra Hearts of Oak",
+    description:
+      "The App will serve as the official app for the Accra Hearts of Oak Sporting Club and serve the fans with news updates, video streams of training sessions and live matches, as well as manage supporter union memberships data.",
+    images: [hearts1, hearts2, hearts3, hearts4],
+    google_play_link:
+      "https://play.google.com/store/apps/details?id=com.heartsofoak.app",
+    apple_appstore_link:
+      "https://apps.apple.com/us/app/farmhouse-movies/id1541154245",
+  },
+  {
+    title: "Lystn (Beta)",
+    description:
+      "Lystn brings personality back into online dating, allowing you to listen to people’s voices before you see their pictures." +
+      "Enter Lystn, a new relationship app which brings personality and happiness back into online dating. We have built the Lystn experience around voice," +
+      " because we believe that the best way of making real connections is by speaking up. Create your own audio recording and introduce yourself like you would on a real first date." +
+      " Spark your curiosity by listening to others" +
+      "By the way; we know that looks matter, too, so don’t worry – you’ll get to see pictures as well. You just need to listen first. Because love might just be in the ear.  ",
+    images: [lystn1, lystn2, lystn3, lystn4],
+    google_play_link:
+      "https://play.google.com/store/apps/details?id=com.jubelapp.lystn",
+    apple_appstore_link:
+      "https://apps.apple.com/us/app/farmhouse-movies/id1541154245",
+  },
+  {
+    title: "Godlives (Internal Beta)",
+    description:
+      "With Godlives, you are covered with delivery of your favourite dishes right to your door step. Check out menu from restaurants near you, order and pay right from the comfort of you home or office." +
+      " We deliver food at lightening speed with a smile.",
+    images: [godlives1, godlives2, godlives3, godlives4],
+    google_play_link:
+      "https://play.google.com/store/apps/details?id=com.godlivesdelivery.userapp",
     apple_appstore_link:
       "https://apps.apple.com/us/app/farmhouse-movies/id1541154245",
   },
