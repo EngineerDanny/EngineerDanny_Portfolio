a1a6f243b68e2adfd9009bd036a31e1a8178528d
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-12-20 19:34:41 +0000

    new updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import profilePic from "../assets/engineerdanny.png";
+import myResume from "../assets/resume.pdf"
 
 export default function HeroSection() {
   return (
@@ -54,7 +55,7 @@ export default function HeroSection() {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href="/#/resume"
+                    href={myResume}
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Résumé.
