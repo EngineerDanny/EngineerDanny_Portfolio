526f95d04a82ad33281a3e0008cdfb87c1526199
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-12-21 16:37:03 +0000

    ProjectTile minor UI updates

--- src/components/Projects/ProjectTile.jsx
+++ src/components/Projects/ProjectTile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import google_play from "../../assets/badges/google-play-badge.png";
-import appstore_badge from "../../assets/badges/appstore-badge.png";
+import google_play from "../../assets/badges/google-play-48.png";
+import apple_logo from "../../assets/badges/apple-logo-100.png";
 import "pure-react-carousel/dist/react-carousel.es.css";
 import Carousel from "./Carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
@@ -25,25 +25,43 @@ function ProjectTile({ projectModel, index }) {
             <h2 className="text-xl py-4 md:py-8 text-gray-400">
               {projectModel.description}
             </h2>
-            <div className="h-28 md:w-4/6  flex flex-row object-center items-center content-start">
-              <div className=" md:h-20 sm:h-28  w-5/6 sm:w-4/6 transform  transition duration-500 hover:scale-105 ">
-                <a href={projectModel.google_play_link}>
+
+            <div className="flex flex-col md:flex-row md:space-x-4 md:space-y-0 space-y-3 mb-5">
+              <a
+                href={projectModel.google_play_link}
+                className="cursor-pointer w-56 bg-black px-3 py-2 border-white rounded-xl border-solid border-2 transform  transition duration-500 hover:scale-105"
+              >
+                <div className=" flex items-center">
                   <img
                     alt="Get it on Google Play"
                     src={google_play}
-                    className="h-20  "
+                    className="h-12 mr-1"
                   />
-                </a>
-              </div>
-              <div className="h-14 w-full transform  transition duration-500 hover:scale-105">
-                <a href={projectModel.apple_appstore_link}>
+                  <div className=" flex flex-col items-start">
+                    <span>GET IT ON</span>
+                    <span className=" text-2xl font-bold">Google Play</span>
+                  </div>
+                </div>
+              </a>
+
+              <a
+                href={projectModel.apple_appstore_link}
+                className={`cursor-pointer w-56 bg-black px-3 py-2 border-white rounded-xl border-solid border-2 transform  transition duration-500 hover:scale-105 ${
+                  projectModel.apple_appstore_link == null ? " hidden" : "block"
+                } `}
+              >
+                <div className=" flex items-center">
                   <img
-                    alt="Get it on Apple Appstore"
-                    src={appstore_badge}
-                    className="h-full"
+                    alt="Get it on Appstore"
+                    src={apple_logo}
+                    className="h-12 mr-1 filter-grayscale "
                   />
-                </a>
-              </div>
+                  <div className=" flex flex-col items-start">
+                    <span>Download on the</span>
+                    <span className=" text-2xl font-bold">App Store</span>
+                  </div>
+                </div>
+              </a>
             </div>
           </div>
         </div>
